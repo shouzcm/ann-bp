c08791bcaf3f596e2d1cf2443fecaa96c969ea57
钟昌明 <zhongchangming@@meituan.com>
2017-12-04 20:36:49 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/ann_bp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ann_bp.dir/demo02/BpNet.cpp.o"
+  "CMakeFiles/ann_bp.dir/demo02/main.cpp.o"
+  "ann_bp.pdb"
+  "ann_bp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ann_bp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
